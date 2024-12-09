4ba8e4bac2a832f4e2c718426895203be2362d05
jarray-kais <jarraykais1@gmail.com>
2024-12-09 23:12:53 +0100

    userlist test
    Signed-off-by: jarray-kais <jarraykais1@gmail.com>

--- tests/components/UserList.test.tsx
+++ tests/components/UserList.test.tsx
@@ -1,20 +1,27 @@
 import { render, screen } from '@testing-library/react'
 import UserList from '../../src/components/UserList'
+import { User } from '../../src/entities'
 
 describe('UserList', () => {
-    it('should render "No users available." when users list is empty', () => {
+    it('should render no users when users list is empty', () => {
         render(<UserList users={[]} />)
-        const message = screen.getByText("No users available.");
+        const message = screen.getByText(/no users/i);
         expect(message).toBeInTheDocument();
     })
 
     it('should render a list of users when provided', () => {
-        const mockusers = [
+        const users: User[] = [
             { id: 1, name: 'kais' },
             { id: 2, name: 'ahmed' },
         ]
-        render(<UserList users={mockusers} />)
-        const listItems = screen.getAllByRole('listitem')
-        expect()
+        render(<UserList users={users} />)
+        const listItems = screen.getAllByRole('listitem');
+        expect(listItems).toHaveLength(users.length);
+        users.forEach((user) => {
+            const link = screen.getByText(user.name)
+            expect(link).toBeInTheDocument()
+            expect(link).toHaveAttribute('href', `/users/${user.id}`)
+
+        })
     })
 })
