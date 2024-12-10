e617cdabcb43a57789a1ca9bb102cb9f6439365e
jarray-kais <jarraykais1@gmail.com>
2024-12-10 20:17:31 +0100

    updated expandaple text
    Signed-off-by: jarray-kais <jarraykais1@gmail.com>

--- tests/components/ExpandableText.test.tsx
+++ tests/components/ExpandableText.test.tsx
@@ -1,7 +1,12 @@
-import { fireEvent, render, screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import ExpandableText from '../../src/components/ExpandableText'
+import { userEvent } from '@testing-library/user-event';
 
 describe('ExpandableText', () => {
+
+    const limit = 255;
+    const longText = 'A'.repeat(300)
+    const truncatedText = longText.substring(0 , limit)
     it('should render the full text if less than or equal to 255 characters ', () => {
         const shortText = 'this is a short text'
         render(<ExpandableText text={shortText}/>)
@@ -12,29 +17,29 @@ describe('ExpandableText', () => {
         expect(screen.queryByRole('button')).not.toBeInTheDocument()
     })
     it('should render truncated text and a show More  button if the text is more than 255 characters', () => {
-        const longText = 'A'.repeat(300)
+        
         render(<ExpandableText text={longText}/>)
         //verfier que le texte tranquée est afficher
-        expect(screen.getByText(`${longText.substring(0 , 255)}...`)).toBeInTheDocument()
+        expect(screen.getByText(`${truncatedText}...`)).toBeInTheDocument()
 
         //verifier que le button show more est afficher 
         expect(screen.getByRole('button', {name: /Show More/i})).toBeInTheDocument()
     })
 
-    it('should render the full text and a show less button if the text is more than 255 characters', () => {
-        const longText = 'A'.repeat(300)
+    it('should render the full text and a show less button if the text is more than 255 characters', async() => {
         render(<ExpandableText text={longText}/>)
         const button = screen.getByRole('button' , {name: /Show More/i})
         //cliquer sur show more button
-        fireEvent.click(button)
+        const user = userEvent.setup()
+        await user.click(button)
         //verifier que le texte complet est rendu
         expect(screen.getByText(longText)).toBeInTheDocument()
 
         //cliquer sur show less button 
-        fireEvent.click(button)
+        await user.click(button)
 
         //verifier que le texte est de nouveau tronqué
-        expect(screen.getByText(`${longText.substring(0, 255)}...`)).toBeInTheDocument()
+        expect(screen.getByText(`${truncatedText}...`)).toBeInTheDocument()
         //verifier que le button show More est afficher
         expect(button).toHaveTextContent(/show more/i)
         
