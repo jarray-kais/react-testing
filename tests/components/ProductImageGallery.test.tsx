482aefd394a0a34684d33a4631a783d72a93a310
jarray-kais <jarraykais1@gmail.com>
2024-12-10 12:16:15 +0100

    commenter
    Signed-off-by: jarray-kais <jarraykais1@gmail.com>

--- tests/components/ProductImageGallery.test.tsx
+++ tests/components/ProductImageGallery.test.tsx
@@ -1,22 +1,26 @@
-import { render, screen } from '@testing-library/react'
-import ProductImageGallery from '../../src/components/ProductImageGallery'
+import { render, screen } from "@testing-library/react";
+import ProductImageGallery from "../../src/components/ProductImageGallery";
 
-describe('ProductImageGallery', () => {
-    it('should render nothing if given an empty array', () => {
-        const {container} =render(<ProductImageGallery imageUrls={[]}/>)
-        
-        expect(container).toBeEmptyDOMElement()
-    })
+describe("ProductImageGallery", () => {
+  it("should render nothing if given an empty array", () => {
+    const { container } = render(<ProductImageGallery imageUrls={[]} />);
 
-    it('should render a list of images', () => {
-        const imageUrls = ['image1.jpg', 'image2.jpg', 'image3.jpg']
-         render(<ProductImageGallery imageUrls={imageUrls}/>)
-        
-        const images = screen.getAllByRole('img')
-        expect(images).toHaveLength(imageUrls.length)
-        imageUrls.forEach((url, index) => {
-            expect(images[index]).toHaveAttribute('src', url)
-        })
-        
-    })
-})
+    expect(container).toBeEmptyDOMElement();
+  });
+
+  it("should render a list of images", () => {
+    // Crée un tableau contenant les URL des images à tester.
+    const imageUrls = ["image1.jpg", "image2.jpg", "image3.jpg"];
+    // Rend le composant `ProductImageGallery` avec les URL fournies.
+    render(<ProductImageGallery imageUrls={imageUrls} />);
+    // Récupère tous les éléments avec le rôle "img" dans le DOM rendu.
+    // Cela correspond aux balises `<img>` générées par le composant.
+    const images = screen.getAllByRole("img");
+     // Vérifie que le nombre d'images affichées est égal au nombre d'URL dans le tableau `imageUrls`.
+    expect(images).toHaveLength(imageUrls.length);
+    // Vérifie que chaque image rendue a l'attribut `src` correspondant à l'URL attendue.
+    imageUrls.forEach((url, index) => {
+      expect(images[index]).toHaveAttribute("src", url);
+    });
+  });
+});
